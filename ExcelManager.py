d4d3e0ab5caf911f5e7ea5d422d90e0576da416b
TweekXCraig <90104262+TweekXCraig@users.noreply.github.com>
2023-03-27 17:01:21 +0200

    card class separation
    improved some stuff, separated the card class from the excelmanager class. This makes card_class.py the third script in the project :)

--- ExcelManager.py
+++ ExcelManager.py
@@ -1,29 +1,9 @@
 import openpyxl
+from card_class import Card
 import time
 
 
 class ExcelManager:
-    class Card:
-        def __init__(self, values):
-            self.level = values[0]
-            self.A = values[1]
-            self.B = values[2]
-            self.extra = values[3]
-            self.extraC = values[4]
-            self.lastRevision = values[5]
-            self.saveLine = values[6]
-
-        def update(self, b_was_right):
-            self.lastRevision = time.time()
-            if b_was_right:
-                self.level = self.level + 1
-            else:
-                if self.level > 1:
-                    self.level = self.level + 1
-
-        def get_values_to_save(self):
-            values = [self.level, self.A, self.B, self.extra, self.extraC, self.lastRevision]
-            return values
 
     def __init__(self, filename):
         self.filename = filename
